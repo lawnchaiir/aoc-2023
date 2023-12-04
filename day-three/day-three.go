6d2da402f3cffd359765aaa8743343e1c34e3716
lawnchaiir <lawnchaiir@users.noreply.github.com>
2023-12-03 20:51:40 -0500

    Day 3 solved!

--- day-three/day-three.go
+++ day-three/day-three.go
@@ -9,51 +9,81 @@ import (
 	"unicode"
 )
 
+type Pos struct {
+	X int
+	Y int
+}
+
 func main() {
 
 	lines := loadInput()
-
 	sum := 0
+	gears := make(map[Pos][]int)
 
 	for lineIdx, line := range lines {
 		isParsingNumber := false
 		currentNumber := ""
 		partAdjacent := false
+		var currentGear *Pos
+
 		for charIdx, r := range line {
 			isDigit := unicode.IsDigit(r)
 
+			// accumulate the digits as we go, and check each one for adjacent parts
 			if isDigit {
 				isParsingNumber = true
 				currentNumber += string(r)
 
 				if !partAdjacent {
+					part, pos := getAdjacentPart(lineIdx, lines, charIdx, line)
 
-					if checkAdjacentParts(lineIdx, lines, charIdx, line) {
+					if part != 0 {
 						partAdjacent = true
+
+						if part == '*' {
+							currentGear = &pos
+						}
 					}
 				}
 			}
 
+			// we're at the end of a number, so flush and reset
 			if (isParsingNumber && !isDigit) || charIdx == len(line)-1 {
-
 				isParsingNumber = false
 
-				if i, err := strconv.Atoi(currentNumber); err == nil {
-
-					if partAdjacent {
+				if partAdjacent {
+					if i, err := strconv.Atoi(currentNumber); err == nil {
 						sum += i
+
+						if currentGear != nil {
+							gearPartNumbers, ok := gears[*currentGear]
+							if !ok {
+								gearPartNumbers = make([]int, 0)
+							}
+							gearPartNumbers = append(gearPartNumbers, i)
+							gears[*currentGear] = gearPartNumbers
+						}
 					}
 				}
 
 				currentNumber = ""
 				partAdjacent = false
+				currentGear = nil
 			}
+		}
+	}
+	fmt.Println(sum)
+
+	sum = 0
+	for _, gearPartNumbers := range gears {
+		if len(gearPartNumbers) == 2 {
+			sum += (gearPartNumbers[0] * gearPartNumbers[1])
 		}
 	}
 	fmt.Println(sum)
 }
 
-func checkAdjacentParts(lineIdx int, lines []string, charIdx int, line string) bool {
+func getAdjacentPart(lineIdx int, lines []string, charIdx int, line string) (byte, Pos) {
 	for offsetX := -1; offsetX <= 1; offsetX++ {
 		x := lineIdx + offsetX
 
@@ -74,13 +104,13 @@ func checkAdjacentParts(lineIdx int, lines []string, charIdx int, line string) b
 
 			char := lines[x][y]
 			if char != '.' && !unicode.IsDigit(rune(char)) {
-				return true
+				return char, Pos{x, y}
 			}
 
 		}
 
 	}
-	return false
+	return 0, Pos{0, 0}
 }
 
 func loadInput() []string {
